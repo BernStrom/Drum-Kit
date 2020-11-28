de4670058959985c9c2cf1c3dd358779efbc8601
Bern <bernnystrom@gmail.com>
2020-11-28 20:54:42 +0800

    Add more comments to describe code functionality

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const playSound = event => {
     audio.currentTime = 0; // Rewinds to the start of the sound.
     audio.play(); // Plays the sound.
 
-    key.classList.add('playing');
+    key.classList.add('playing'); // Adds the playing style class to the key.
 }
 
 function removeTransition(event) {
     if (event.propertyName !== 'transform') return; // Skip it if it's not a transform.
-    this.classList.remove('playing');
+    this.classList.remove('playing'); // Removes the playing style class from the key.
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
