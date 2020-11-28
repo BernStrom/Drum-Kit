9488dc299f211396a3cecd72a94ad5f9f73b2654
Bern <bernnystrom@gmail.com>
2020-11-28 20:48:24 +0800

    Remove console logs from script file

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const playSound = event => {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-    // console.log(audio);
-    // console.log(key);
 
     if (!audio) return; // Stops the function from running all together.
     audio.currentTime = 0; // Rewinds to the start of the sound.
@@ -11,15 +9,11 @@ const playSound = event => {
     key.classList.add('playing');
 }
 
- function removeTransition(event) {
-    // console.log(event);
+function removeTransition(event) {
     if (event.propertyName !== 'transform') return; // Skip it if it's not a transform.
-    // console.log(this);
     this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 window.addEventListener('keydown', playSound);
-
-// console.log(keys);
